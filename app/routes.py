c8e688132d300bb87584c731c244e1e7c4e57abc
Shelton Gayles <sgbythelake@gmail.com>
2020-02-22 13:13:11 -0800

    removed sample location fields from login

--- app/routes.py
+++ app/routes.py
@@ -24,7 +24,7 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}, location={}'.format(
-            form.username.data, form.remember_me.data, form.location.data))
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
