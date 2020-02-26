b15e6b953e1ed950435c7209d694c457f8d8630e
Shelton Gayles <sgbythelake@gmail.com>
2020-02-26 01:35:19 -0800

    email almost working

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordForm, ResetPasswordRequestForm
 from flask_login import current_user, login_user, login_required, logout_user
 from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
+from app.email import send_password_reset_email
 
 
 @app.before_request
@@ -29,8 +30,10 @@ def index():
     posts = current_user.followed_posts().paginate(
         page, app.config["POSTS_PER_PAGE"], False
     )
-    next_url = url_for("index", page=posts.next_num) if posts.has_next else None
-    prev_url = url_for("index", page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for(
+        "index", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for(
+        "index", page=posts.prev_num) if posts.has_prev else None
     return render_template(
         "index.html",
         title="Home",
@@ -48,8 +51,10 @@ def explore():
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config["POSTS_PER_PAGE"], False
     )
-    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
-    prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for(
+        "explore", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for(
+        "explore", page=posts.prev_num) if posts.has_prev else None
     return render_template(
         "index.html",
         title="Explore",
@@ -156,3 +161,33 @@ def unfollow(username):
     flash("You are not following {}.".format(username))
     return redirect(url_for("user", username=username))
 
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
